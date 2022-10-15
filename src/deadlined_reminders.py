667b14acc28173e59f2e40792b939825788334bb
erickrex <erickrhein@gmail.com>
2022-10-15 23:29:54 +0200

    fix: Properly inheriting from more than 1 class

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,14 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 
 
-class DeadlinedMetaReminder(Iterable):
-    def __init__(self, metaclass=ABCMeta):
-        self.metaclass = metaclass
-
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta ):
     @abstractmethod
     def is_due(self):
         pass
 
-
+class DeadlinedReminder(ABC, Iterable):
+    @abstractmethod
+    def is_due(self):
+        pass
 
