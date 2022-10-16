898c1cb55af65203f05219cd8165d7ec41aa5e90
erickrex <erickrhein@gmail.com>
2022-10-16 02:12:51 +0200

    feat: 9 Make add_reminder() accept a conforming reminder

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.database import add_reminder, list_reminders
+from src.deadlined_reminders import DateReminder
 
 
 def handle_input():
@@ -12,13 +13,14 @@ def handle_input():
     elif(choice == "2"):
         print()
         reminder = input("What would you like to be reminded about?: ")
-
-        add_reminder(reminder)
+        date = input("When is that due?: ")
+        add_reminder(reminder, date, DateReminder)
         list_reminders()
     else:
         print("Invalid menu option")
 
     return True
+
 
 def print_menu():
     print()
@@ -34,10 +36,12 @@ def print_menu():
     print('2) Add a reminder')
     print('3) Exit')
 
+
 def main():
     print_menu()
     while handle_input():
         print_menu()
+
 
 if __name__ == '__main__':
     main()
